12262190e64219abc6fdd0f56a5a2c27d33fcb0d
ZekiaBey <zekiabyne@gmail.com>
2026-01-18 18:16:46 -0500

    Add Javadoc to Fibonacci method (fixes #1)

--- scr/Fibonacci.java
+++ scr/Fibonacci.java
@@ -1,8 +1,16 @@
 public class Fibonacci {
 
-    // Recursive static method that returns the nth Fibonacci number.
-    // Definition used:
-    // fib(0) = 0, fib(1) = 1, fib(n) = fib(n - 1) + fib(n - 2)
+    /**
+     * Calculates the nth term of the Fibonacci sequence using recursion.
+     * <p>
+     * The Fibonacci sequence is defined as:
+     * fib(0) = 0, fib(1) = 1, and fib(n) = fib(n - 1) + fib(n - 2) for n >= 2.
+     * </p>
+     *
+     * @param n the index (0 or greater) of the Fibonacci term to compute
+     * @return the nth Fibonacci number
+     * @throws IllegalArgumentException if n is negative
+     */
     public static int fib(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("n must be 0 or greater.");
